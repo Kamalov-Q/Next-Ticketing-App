0febd3de03fb9aeefbfa819ab7ce4872abdf2345
Kamalov Q <kamolovquvomiddin9@gmail.com>
2025-03-25 02:53:38 +0500

    Changes applied

--- app/ticket-page/api/tickets/[id]/route.ts
+++ app/ticket-page/api/tickets/[id]/route.ts
@@ -1,16 +1,16 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export const DELETE = async (
-  req: Request,
-  { params }: { params: { id: string } }
-) => {
-  try {
-    const { id } = params;
-  } catch (error: any) {
-    console.error(error);
-    return NextResponse.json(
-      { message: "Internal Server Error", error, data: null },
-      { status: 500 }
-    );
-  }
-};
+// export const DELETE = async (
+//   req: Request,
+//   { params }: { params: { id: string } }
+// ) => {
+//   try {
+//     const { id } = params;
+//   } catch (error: any) {
+//     console.error(error);
+//     return NextResponse.json(
+//       { message: "Internal Server Error", error, data: null },
+//       { status: 500 }
+//     );
+//   }
+// };
